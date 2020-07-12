0a547e9d864d7a79963cdbdd0bd33830f2661b54
hong <gmm117@naver.com>
2020-07-12 15:08:51 +0900

    webpack config update

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -37,8 +37,7 @@ module.exports = {
   },
   resolve: {
     modules: [
-      path.join(__dirname, "src"),
-      "node_modules"
+      path.join(__dirname, "src")
     ],
     alias: {
       'calc': './modules/calc'
